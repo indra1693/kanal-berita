2323f7f39d21c93ced8b3d4f2d20ed39511ad79b
indra1693 <indra160293@gmail.com>
2023-11-14 07:15:22 +0700

    menambah data berita

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class postController extends Controller
@@ -12,5 +13,29 @@ class postController extends Controller
     public function index() : View {
         $posts = Post::latest()->paginate(5);
         return view('posts.index', compact('posts'));
+    }
+
+    public function create() : view {
+        return view('posts.create');
+        
+    }
+
+    public function store(Request $request) : RedirectResponse {
+        $this->validate($request, [
+            'image' => 'required|image|mimes:png,jpg,jpeg|max:2048',
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        Post::create([
+            'image' => $image->hasName(),
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil di Simpan']);
     }
 }
